c3f898273ad902323bd6e6eb88c04d0e9eefcd6d
aankor <lambdasound@yandex.ru>
2022-08-23 17:30:18 +0200

    SplGovDataMiddleware.ts -> SplGovernanceMiddleware.ts

---
+++ packages/solana-cli-utils/middleware/multisig/SplGovernanceMiddleware.ts
@@ -0,0 +1,133 @@
+import { Provider, TransactionEnvelope } from '@saberhq/solana-contrib';
+import { PublicKey, Signer } from '@solana/web3.js';
+import { MultisigMiddlewareBase } from './MultisigMiddlewareBase';
+import {
+  createInstructionData,
+  getGovernance,
+  getRealm,
+  getTokenOwnerRecord,
+  getTokenOwnerRecordAddress,
+  Governance,
+  ProgramAccount,
+  PROGRAM_VERSION,
+  Realm,
+  serializeInstructionToBase64,
+  VoteType,
+  withCreateProposal,
+  withInsertTransaction,
+} from '@solana/spl-governance';
+
+export class SplGovernanceMiddleware extends MultisigMiddlewareBase {
+  private constructor(
+    public readonly provider: Provider,
+    public readonly goverance: ProgramAccount<Governance>,
+    public readonly realm: ProgramAccount<Realm>,
+    public readonly proposer: Signer | PublicKey,
+    public readonly rentPayer: Signer | PublicKey,
+    public readonly approvers: (Signer | PublicKey)[],
+    public readonly logOnly: boolean
+  ) {
+    super();
+  }
+
+  static readonly PROG_ID = new PublicKey(
+    'GovER5Lthms3bLBqWub97yVrMmEogzX7xNjdXpPPCVZw'
+  );
+
+  static async create({
+    provider,
+    account,
+    proposer = provider.wallet.publicKey,
+    rentPayer = provider.wallet.publicKey,
+    approvers = [],
+    logOnly = false,
+  }: {
+    provider: Provider;
+    account: PublicKey;
+    proposer?: Signer | PublicKey;
+    rentPayer?: Signer | PublicKey;
+    approvers?: (Signer | PublicKey)[];
+    logOnly?: boolean;
+  }) {
+    const goverance = await getGovernance(provider.connection, account);
+    const realm = await getRealm(provider.connection, goverance.account.realm);
+    return new SplGovernanceMiddleware(
+      provider,
+      goverance,
+      realm,
+      proposer,
+      rentPayer,
+      approvers,
+      logOnly
+    );
+  }
+
+  async createTransaction(
+    inner: TransactionEnvelope
+  ): Promise<TransactionEnvelope> {
+    if (this.logOnly) {
+      console.log('Instructions:');
+      for (const ix of inner.instructions) {
+        console.log('  ' + serializeInstructionToBase64(ix));
+      }
+      return Promise.resolve(new TransactionEnvelope(inner.provider, []));
+    }
+
+    const proposerKey =
+      this.proposer instanceof PublicKey
+        ? this.proposer
+        : this.proposer.publicKey;
+    const rentPayerKey =
+      this.rentPayer instanceof PublicKey
+        ? this.rentPayer
+        : this.rentPayer.publicKey;
+    const tokenOwnerRecord = await getTokenOwnerRecordAddress(
+      SplGovernanceMiddleware.PROG_ID,
+      this.goverance.account.realm,
+      this.realm.account.config.councilMint!,
+      proposerKey
+    );
+    const tx = new TransactionEnvelope(inner.provider, []);
+    const proposal = await withCreateProposal(
+      tx.instructions,
+      SplGovernanceMiddleware.PROG_ID,
+      PROGRAM_VERSION,
+      this.goverance.account.realm,
+      this.goverance.pubkey,
+      tokenOwnerRecord,
+      Math.random().toString(), // TODO
+      '',
+      this.realm.account.config.councilMint!,
+      proposerKey,
+      this.goverance.account.proposalCount,
+      VoteType.SINGLE_CHOICE,
+      ['approve'],
+      true,
+      rentPayerKey,
+      undefined
+    );
+    const proposalTransaction = await withInsertTransaction(
+      tx.instructions,
+      SplGovernanceMiddleware.PROG_ID,
+      PROGRAM_VERSION,
+      this.goverance.pubkey,
+      proposal,
+      tokenOwnerRecord,
+      proposerKey,
+      this.goverance.account.proposalCount,
+      0,
+      0,
+      inner.instructions.map(createInstructionData),
+      rentPayerKey
+    );
+    return tx;
+  }
+
+  get programId() {
+    return SplGovernanceMiddleware.PROG_ID;
+  }
+
+  get signingBy() {
+    return this.goverance.pubkey;
+  }
+}
--- packages/solana-cli-utils/middleware/multisig/index.ts
+++ packages/solana-cli-utils/middleware/multisig/index.ts
@@ -2,10 +2,10 @@ import { GokiSDK } from '@gokiprotocol/client';
 import { Keypair, PublicKey } from '@solana/web3.js';
 import { Middleware } from '..';
 import { GokiMiddleware } from './GokiMiddleware';
-import { SplGovDataMiddleware } from './SplGovDataMiddleware';
+import { SplGovernanceMiddleware } from './SplGovernanceMiddleware';
 
 export { GokiMiddleware } from './GokiMiddleware';
-export { SplGovDataMiddleware } from './SplGovDataMiddleware';
+export { SplGovernanceMiddleware as SplGovDataMiddleware } from './SplGovernanceMiddleware';
 
 export async function installMultisigMiddleware({
   middleware,
@@ -13,12 +13,14 @@ export async function installMultisigMiddleware({
   address,
   proposer,
   rentPayer,
+  logOnly,
 }: {
   middleware: Middleware[];
   goki: GokiSDK;
   address: PublicKey;
   proposer?: Keypair;
   rentPayer?: Keypair;
+  logOnly?: boolean;
 }) {
   const account = await goki.provider.getAccountInfo(address);
   if (account) {
@@ -31,10 +33,16 @@ export async function installMultisigMiddleware({
           rentPayer,
         })
       );
-    } else if (account.accountInfo.owner.equals(SplGovDataMiddleware.PROG_ID)) {
+    } else if (
+      account.accountInfo.owner.equals(SplGovernanceMiddleware.PROG_ID)
+    ) {
       middleware.push(
-        await SplGovDataMiddleware.create({
+        await SplGovernanceMiddleware.create({
+          provider: goki.provider,
           account: address,
+          proposer,
+          rentPayer,
+          logOnly,
         })
       );
     }
