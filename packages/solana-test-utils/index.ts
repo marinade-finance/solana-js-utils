f730e75266d002f6b68ee9c66234ff4c78c33601
phola <info@phola.com>
2022-08-23 18:39:33 +0100

    feat: added mint and runner

--- packages/solana-test-utils/index.ts
+++ packages/solana-test-utils/index.ts
@@ -1,5 +1,6 @@
 import shellMatchers from 'jest-shell-matchers';
-
+export * from './mint';
+export * from './runner';
 export { createFileKeypair } from './fileKeypair';
 
 export { shellMatchers };
