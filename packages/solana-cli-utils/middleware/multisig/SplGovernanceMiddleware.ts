72a191101a5d6ddd8e011f403095e542c603a906
aankor <lambdasound@yandex.ru>
2022-08-23 18:00:08 +0200

    Fix withSignOffProposal

--- packages/solana-cli-utils/middleware/multisig/SplGovernanceMiddleware.ts
+++ packages/solana-cli-utils/middleware/multisig/SplGovernanceMiddleware.ts
@@ -131,7 +131,7 @@ export class SplGovernanceMiddleware extends MultisigMiddlewareBase {
       proposal,
       proposerKey,
       undefined,
-      undefined
+      tokenOwnerRecord
     );
     return tx;
   }
